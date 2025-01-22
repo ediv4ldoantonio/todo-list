691f3fa12694a20e9a3d7f324edc19cfe5096aba
Edivaldo António <ediv4ldoantonio@gmail.com>
2025-01-22 23:28:46 +0100

    feat - adiconado o componente para o todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoComponent } from './components/todo/todo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TodoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todo-list';
--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../models/task.model';
+import { TaskItemComponent } from '../task-item/task-item.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
-  imports: [],
   templateUrl: './todo.component.html',
-  styleUrl: './todo.component.css'
+  styleUrls: ['./todo.component.css'],
+  standalone: true,
+  imports: [TaskItemComponent, FormsModule],
 })
 export class TodoComponent {
+  tasks: Task[] = [];
+  newTask: string = '';
 
+  constructor(private taskService: TaskService) {
+    this.tasks = this.taskService.getTasks();
+  }
+
+  addTask(): void {
+    if (this.newTask.trim()) {
+      this.taskService.addTask(this.newTask.trim());
+      this.newTask = '';
+      this.tasks = this.taskService.getTasks();
+    }
+  }
+
+  deleteTask(id: number): void {
+    this.taskService.deleteTask(id);
+    this.tasks = this.taskService.getTasks();
+  }
+
+  toggleTask(id: number): void {
+    this.taskService.toggleTaskCompletion(id);
+    this.tasks = this.taskService.getTasks();
+  }
+
+  editTask({ id, title }: { id: number; title: string }): void {
+    this.taskService.updateTask(id, title);
+    this.tasks = this.taskService.getTasks();
+  }
 }
