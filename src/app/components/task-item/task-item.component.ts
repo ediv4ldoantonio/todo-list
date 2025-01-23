52e2694a53de0158cdc1c70f77fc2b87a82dc748
Edivaldo António <ediv4ldoantonio@gmail.com>
2025-01-23 11:51:06 +0100

    feat - adicionado o estilo para o task-item

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../models/task.model';
 
 @Component({
-  selector: 'app-task-item',
+  selector: 'task-item',
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.css'],
   standalone: true,
