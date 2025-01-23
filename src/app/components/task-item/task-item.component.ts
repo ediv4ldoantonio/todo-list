ec1c0c4ef714f4e96da2f3b2389c59b4b45032be
Edivaldo António <ediv4ldoantonio@gmail.com>
2025-01-23 16:00:31 +0100

    ref - atualizar o README

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -7,6 +7,15 @@ import {
 } from '@angular/core';
 import { Task } from '../../models/task.model';
 
+/**
+ * Componente que representa um item de tarefa.
+ *
+ * @selector 'task-item'
+ * @templateUrl './task-item.component.html'
+ * @styleUrls ['./task-item.component.css'
+ * @standalone true
+ * @encapsulation ViewEncapsulation.None
+ */
 @Component({
   selector: 'task-item',
   templateUrl: './task-item.component.html',
@@ -15,14 +24,33 @@ import { Task } from '../../models/task.model';
   encapsulation: ViewEncapsulation.None,
 })
 export class TaskItemComponent {
+  /**
+   * A tarefa a ser exibida.
+   */
   @Input() task!: Task;
+
+  /**
+   * Evento emitido quando a tarefa é deletada.
+   */
   @Output() delete = new EventEmitter<number>();
+
+  /**
+   * Evento emitido quando o status da tarefa é alternado.
+   */
   @Output() toggle = new EventEmitter<number>();
 
+  /**
+   * Método chamado ao deletar a tarefa.
+   * Emite o evento de deletar com o ID da tarefa.
+   */
   onDelete(): void {
     this.delete.emit(this.task.id);
   }
 
+  /**
+   * Método chamado ao alternar o status da tarefa.
+   * Emite o evento de alternar com o ID da tarefa.
+   */
   onToggle(): void {
     this.toggle.emit(this.task.id);
   }
--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -8,6 +8,16 @@ import {
   CdkDragDrop,
   moveItemInArray,
 } from '@angular/cdk/drag-drop';
+
+/**
+ * Componente que representa a lista de tarefas.
+ *
+ * @selector 'app-todo'
+ * @templateUrl './todo.component.html'
+ * @styleUrls ['./todo.component.css']
+ * @standalone true
+ * @encapsulation ViewEncapsulation.None
+ */
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -24,6 +34,9 @@ export class TodoComponent {
     this.tasks = this.taskService.getTasks();
   }
 
+  /**
+   * Adiciona uma nova tarefa.
+   */
   addTask(): void {
     if (this.newTask.trim()) {
       this.taskService.addTask(this.newTask.trim());
@@ -32,29 +45,53 @@ export class TodoComponent {
     }
   }
 
+  /**
+   * Deleta uma tarefa pelo ID.
+   * @param id - ID da tarefa a ser deletada.
+   */
   deleteTask(id: number): void {
     this.taskService.deleteTask(id);
     this.tasks = this.taskService.getTasks();
   }
 
+  /**
+   * Alterna o estado de conclusão de uma tarefa pelo ID.
+   * @param id - ID da tarefa a ser alternada.
+   */
   toggleTask(id: number): void {
     this.taskService.toggleTaskCompletion(id);
     this.tasks = this.taskService.getTasks();
   }
 
+  /**
+   * Edita uma tarefa pelo ID e novo título.
+   * @param param0 - Objeto contendo o ID e o novo título da tarefa.
+   */
   editTask({ id, title }: { id: number; title: string }): void {
     this.taskService.updateTask(id, title);
     this.tasks = this.taskService.getTasks();
   }
 
+  /**
+   * Reordena as tarefas após um evento de arrastar e soltar.
+   * @param event - Evento de arrastar e soltar.
+   */
   drop(event: CdkDragDrop<any[]>): void {
     moveItemInArray(this.tasks, event.previousIndex, event.currentIndex);
   }
 
+  /**
+   * Retorna a contagem de tarefas concluídas.
+   * @returns Número de tarefas concluídas.
+   */
   public get completedTasksCount(): number {
     return this.tasks.filter((task) => task.isCompleted).length;
   }
 
+  /**
+   * Retorna a contagem de tarefas não concluídas.
+   * @returns Número de tarefas não concluídas.
+   */
   public get incompleteTasksCount(): number {
     return this.tasks.filter((task) => !task.isCompleted).length;
   }
